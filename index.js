0ae55b74efc5b7c375847643e2a6ee9444a8cb16
carolina <ci.arayagonzalez@gmail.com>
2024-11-01 23:23:08 -0300

    add-method put

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const url = require("url");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-
 const port = 3000;
 
 http
@@ -22,33 +21,35 @@ http
       if (method == "GET") {
         const paramsObj = urlParsed.query;
 
-        const contentText = fs.readFileSync(dataAnimales, "utf-8")
-        const contentJS = JSON.parse(contentText)
+        const contentText = fs.readFileSync(dataAnimales, "utf-8");
+        const contentJS = JSON.parse(contentText);
 
-        const contentFiltered = contentJS.filter( animal => {
-            if(paramsObj.habitat && paramsObj.habitat == animal.habitat){
-              return true
-            }
-            if(paramsObj.especie && paramsObj.especie == animal.especie){
-              return true
-            }
-            if(!paramsObj.habitat && !paramsObj.especie){
-              return true
-            }
-            return false
-        })
-        console.log(contentFiltered)
+        const contentFiltered = contentJS.filter((animal) => {
+          if (paramsObj.habitat && paramsObj.habitat == animal.habitat) {
+            return true;
+          }
+          if (paramsObj.especie && paramsObj.especie == animal.especie) {
+            return true;
+          }
+          if (!paramsObj.habitat && !paramsObj.especie) {
+            return true;
+          }
+          return false;
+        });
+        console.log(contentFiltered);
 
         res.setHeader("Content-Type", "aplication/json");
-          res.writeHead(200);
-          res.end(
-            JSON.stringify({ message: "Lista de animales", data: contentFiltered })
-          );
-
+        res.writeHead(200);
+        res.end(
+          JSON.stringify({
+            message: "Lista de animales",
+            data: contentFiltered,
+          })
+        );
 
         //POST
       } else if (method == "POST") {
-        let body = ""; 
+        let body = "";
         req.on("data", (chunk) => {
           body = body + chunk.toString();
         });
@@ -56,7 +57,7 @@ http
           body = JSON.parse(body);
           const contentString = fs.readFileSync(dataAnimales, "utf-8");
           const contentJS = JSON.parse(contentString);
-           
+
           const animal = {
             id: uuidv4(),
             nombre: body.nombre,
@@ -66,7 +67,11 @@ http
           };
 
           contentJS.push(animal);
-          fs.writeFileSync(dataAnimales, JSON.stringify(contentJS, null, 2), "utf-8");
+          fs.writeFileSync(
+            dataAnimales,
+            JSON.stringify(contentJS, null, 2),
+            "utf-8"
+          );
           res.setHeader("Content-Type", "aplication/json");
           res.writeHead(201);
           res.end(
@@ -75,13 +80,40 @@ http
         });
         //PUT
       } else if (method == "PUT") {
-        res.end("Actualizar Animales");
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        req.on("end", () => {
+          body = JSON.parse(body);
+
+          const contentTxt = fs.readFileSync(dataAnimales, "utf-8");
+          let contentJS = JSON.parse(contentTxt);
+
+          const index = contentJS.findIndex(
+            (animal) => animal.id == body.id
+          );
+
+          if (index != -1) {
+            body = Object.fromEntries(Object.entries(body).filter(([keyValue]) => keyValue in contentJS[index]))
+            contentJS[index] = { ...contentJS[index], ...body };
+              fs.writeFileSync(
+                dataAnimales,
+                JSON.stringify(contentJS, null, 2),
+                "utf-8"
+              );
+          }
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({ message: "Lista modificada", data: contentJS })
+          );
+        });
+
         //DELETE
       } else if (method == "DELETE") {
         res.end("Eliminar Animales");
       }
     }
-
-    
   })
   .listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`));
