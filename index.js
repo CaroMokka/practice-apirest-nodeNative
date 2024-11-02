861f8962c23866dfa3be7a91e07aa7191466a8e4
carolina <ci.arayagonzalez@gmail.com>
2024-11-02 00:29:40 -0300

    add - validation in method get and put

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ http
         });
         req.on("end", () => {
           body = JSON.parse(body);
+
           const contentString = fs.readFileSync(dataAnimales, "utf-8");
           const contentJS = JSON.parse(contentString);
 
@@ -65,18 +66,24 @@ http
             especie: body.especie,
             habitat: body.habitat,
           };
-
-          contentJS.push(animal);
-          fs.writeFileSync(
-            dataAnimales,
-            JSON.stringify(contentJS, null, 2),
-            "utf-8"
-          );
-          res.setHeader("Content-Type", "aplication/json");
-          res.writeHead(201);
+          if(animal.nombre == body.nombre || animal.especie == body.especie){
+            res.writeHead(404, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ message: "Este animal ya existe dentro del registro" }))
+          } else {
+            contentJS.push(animal);
+            fs.writeFileSync(
+              dataAnimales,
+              JSON.stringify(contentJS, null, 2),
+              "utf-8"
+            );
+            res.writeHead(201, { "Content-Type": "application/json" });
           res.end(
             JSON.stringify({ message: "Registro éxitoso", data: animal })
           );
+          }
+          
+          
+          
         });
         //PUT
       } else if (method == "PUT") {
@@ -90,24 +97,34 @@ http
           const contentTxt = fs.readFileSync(dataAnimales, "utf-8");
           let contentJS = JSON.parse(contentTxt);
 
-          const index = contentJS.findIndex(
-            (animal) => animal.id == body.id
-          );
+          const index = contentJS.findIndex((animal) => animal.id == body.id);
 
           if (index != -1) {
-            body = Object.fromEntries(Object.entries(body).filter(([keyValue]) => keyValue in contentJS[index]))
-            contentJS[index] = { ...contentJS[index], ...body };
+            //Filtro para que no se agreguen nuevas propiedades
+            body = Object.fromEntries(
+              Object.entries(body).filter(
+                ([keyValue]) => keyValue in contentJS[index]
+              )
+            );
+            if (body.nombre == contentJS[index].nombre) {
+              res.writeHead(400, { "Content-Type": "application/json"} )
+              res.end(JSON.stringify({ message: "Este nombre ya existe" }));
+            } else {
+              contentJS[index] = { ...contentJS[index], ...body };
               fs.writeFileSync(
                 dataAnimales,
                 JSON.stringify(contentJS, null, 2),
                 "utf-8"
               );
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify({ message: "Lista modificada", data: contentJS })
+              );
+            }
+          } else {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Animal no encontrado" }));
           }
-
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(
-            JSON.stringify({ message: "Lista modificada", data: contentJS })
-          );
         });
 
         //DELETE
